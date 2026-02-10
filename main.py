8b6ea35d176e43549de5b666fbd6f873ec78d02d
Mohit <mob850580@gmail.com>
2026-02-10 18:12:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+price1 = 30023.3344
+price2 = -4330001.33
+price3 = 1200.3
+
+print(f"Price 1 : ${price1:+010,.2f}")
+print(f"Price 2 : ${price2:+010,.2f}")
+print(f"Price 3 : ${price3:+010,.2f}")
+
